b88064157924c6da9a24da341da2e7e446e72e1f
sulaiman4j <mpsk7492@gmail.com>
2020-11-17 21:45:20 +0330

    initial

---
+++ scraper/src/test/java/com/imdb/scraper/ScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imdb.scraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
